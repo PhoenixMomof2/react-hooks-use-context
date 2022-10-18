b0a7aa52b9a03b9fba54e80739dc6b7cb25672df
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2022-10-18 12:47:21 -0400

    commit tree bc37483a14d29c8ff1ce2abd992ff8aa53c1f3ae

---
+++ src/context/user.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+// create the context
+const UserContext = React.createContext();
+
+//create a provider component
+function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
+  // const currentUser = {
+  //   name: "Duane",
+  //   interests: ["Coding", "Biking", "Words ending in 'ing'"],
+  // };
+  //the value prop of the provider will be our context data
+  // this value will be available to child components of this Provider
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export { UserContext, UserProvider };
